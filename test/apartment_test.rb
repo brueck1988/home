899eaaa793e587330767bb628fd4f61136ca024e
brueck1988 <Brueck1988@gmail.com>
2021-02-17 09:27:12 -0700

    Test created for  apartment class has attributes

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -8,4 +8,14 @@ class ApartmentTest < Minitest::Test
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     assert_instance_of Apartment, unit1
   end
+
+  def test_it_has_attributes
+    renter1 = Renter.new("Jessie")
+    renter1.name
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+  end
 end
