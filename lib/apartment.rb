5acb5f100f7438364e2247b2104a7a4842764093
brueck1988 <Brueck1988@gmail.com>
2021-02-17 09:27:56 -0700

    Apartment class has attributes

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,7 +1,13 @@
 class Apartment
-  attr_reader :unit
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms
 
-def initialize(unit)
-  @unit = unit
+def initialize(unit_info)
+  @number = unit_info[:number]
+  @monthly_rent = unit_info[:monthly_rent]
+  @bathrooms = unit_info[:bathrooms]
+  @bedrooms = unit_info[:bedrooms]
 end
 end
