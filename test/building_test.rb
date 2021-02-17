1603aab6d7dd80a83cf8113e3ba9c26050967039
brueck1988 <Brueck1988@gmail.com>
2021-02-17 09:54:38 -0700

    Attributes added to building class

--- test/building_test.rb
+++ test/building_test.rb
@@ -13,5 +13,10 @@ class BuildingTest < Minitest::Test
   def test_it_has_attributes
     building = Building.new
     assert_equal [], building.units
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    assert_equal [unit1, unit2], building.units
   end
 end
