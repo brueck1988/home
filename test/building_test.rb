199cbe8b4d61efd7036c4dd2402580e9dfcbbdb3
brueck1988 <Brueck1988@gmail.com>
2021-02-17 09:55:49 -0700

    Building test updated for add_unit method

--- test/building_test.rb
+++ test/building_test.rb
@@ -13,6 +13,11 @@ class BuildingTest < Minitest::Test
   def test_it_has_attributes
     building = Building.new
     assert_equal [], building.units
+  end
+
+  def test_add_unit
+    building = Building.new
+    assert_equal [], building.units
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     building.add_unit(unit1)
