50b7db3c6d20ecca949112f96a32af5d6d6fe5f8
brueck1988 <Brueck1988@gmail.com>
2021-02-17 09:46:59 -0700

    Building class test it exists created

--- test/building_test.rb
+++ test/building_test.rb
@@ -5,4 +5,8 @@ require './lib/apartment'
 require './lib/renter'
 
 class BuildingTest < Minitest::Test
+  def test_it_exists
+    building = Building.new
+    assert_instance_of Building, building
+  end
 end
